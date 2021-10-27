393e1462a833d2e0a1b2eeebe8b7dde413a834e3
mironhasan <mironmahmud018@gmail.com>
2021-10-27 12:45:36 +0600

    organe-v1.5

--- assets/ltr/js/accordion.js
+++ assets/ltr/js/accordion.js
@@ -1,1 +1,43 @@
-jQuery(document).ready((function(e){var i=e(".faq-ans").hide();i.first().show(),e(".faq-que").click((function(){var n=e(this);i.slideUp(),n.next().slideDown()}))})),jQuery(document).ready((function(e){var i=e(".orderlist-body").hide();i.first().show(),e(".orderlist-head").click((function(){var n=e(this);i.slideUp(),n.next().slideDown()}))}));
+
+
+
+
+
+
+
+
+// FOR FAQS
+jQuery(document).ready(function($){
+    
+    var panels = $(".faq-ans").hide();
+
+    panels.first().show();
+    
+     $(".faq-que").click(function(){
+
+         var $this = $(this);
+
+         panels.slideUp();
+         $this.next().slideDown();
+         
+    });
+
+});
+
+// FOR ORDER LIST
+jQuery(document).ready(function($){
+    
+    var panels = $(".orderlist-body").hide();
+
+    panels.first().show();
+    
+     $(".orderlist-head").click(function(){
+
+         var $this = $(this);
+
+         panels.slideUp();
+         $this.next().slideDown();
+         
+    });
+
+});
